ae5cada1733d3c27bad3756be95b8a8c7d3d4f32
up1 <somkiat.p@gmail.com>
2022-02-09 13:46:01 +0700

    Demo with php

---
+++ demo-php/README.md
@@ -0,0 +1,12 @@
+# demo-php-ci
+
+```
+//Update library and autoload
+$composer update
+
+//Run web server
+$php -S localhost:9999 -t web/
+
+//Run unit test
+$./vendor/bin/phpunit
+```
---
+++ demo-php/web/index.php
@@ -0,0 +1,9 @@
+<?php
+phpinfo();
+require_once __DIR__.'/../vendor/autoload.php';
+$user_pdo = new UserPDO();
+$users = $user_pdo->getAllUsers();
+foreach ($users as $user) {
+    echo $user->getFirstname() . "-" . $user->getLastname() . "<br/>";
+}
+?>
